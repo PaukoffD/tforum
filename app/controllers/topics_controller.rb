f867e30426c3506498a5cc86afc300f6e9ed3e08
xuliaP <paukov2967@gmail.com>
2015-10-16 15:30:06 +0300

    '5'

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,8 +5,8 @@ before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def index
     
-    @topics = Topic.all
-    topic=Topic.order("created_at DESC").last
+    @topics = Topic.all.page(params[:page])
+    topic=Topic.order(:created_at).reorder('id DESC').last
 	@post = Post.find_by topic_id: topic.id
   end
 
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
  belongs_to :forum
 has_many :posts, :dependent => :destroy
 accepts_nested_attributes_for :posts
+
+self.per_page = 15
 end
