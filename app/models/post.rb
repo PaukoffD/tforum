6d73a326ce710493b2eabd781558230380cfde91
xuliaP <paukov2967@gmail.com>
2015-10-23 13:08:35 +0300

    'last1'

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ActiveRecord::Base
  belongs_to :topic
  delegate :forum, :to => :topic
+ 
+ def last_topic_user
+ post=Post.find_by(topic_id: topic.id)
+ #post=@post.last
+ #@post = Post.find_by topic_id: topic.id
+ user=User.find(post.user_id)
+ user1 = user.name
+ return user1
+end
 end
